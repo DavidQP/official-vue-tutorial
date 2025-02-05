d6bd9d599e784bd3ba72e76b856da3b4981f183e
david <juda9323@gmail.com>
2025-02-05 09:53:05 -0500

    Add getting started

---
+++ src/components/GettingStarted.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref(`Welcome from ${Intl.DateTimeFormat().resolvedOptions().timeZone.split('/').pop()}`)
+</script>
+
+<template>
+  <h2>{{ message }}</h2>
+</template>
