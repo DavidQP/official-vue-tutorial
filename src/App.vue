3309479767b50de3aca3b272dd83e6243931eb6c
david <juda9323@gmail.com>
2025-02-06 12:33:10 -0500

    Add event listening section

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import { ref } from 'vue';
 import GettingStarted from '@/components/GettingStarted.vue'
 import DeclarativeRendering from '@/components/DeclarativeRendering.vue'
 import AttributesBinding from '@/components/AttributesBinding.vue'
+import EventListeners  from '@/components/EventListeners.vue'
+
 const section = ref(0);
 </script>
 
@@ -12,4 +14,5 @@ const section = ref(0);
   <GettingStarted :section="1" />
   <DeclarativeRendering :section="2" />
   <AttributesBinding :section="3" />
+  <EventListeners :section="4" />
 </template>
---
+++ src/components/EventListeners.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  section: {
+    type: Number,
+    default: 0
+  }
+})
+
+const title = ref('Event Listeners')
+const count = ref(0)
+
+// functions
+function increment() {
+  count.value++;
+}
+
+</script>
+
+<template>
+  <h2>{{ section }} - {{ title }}</h2>
+  <button @click="increment">Count is : {{ count }}</button>
+</template>
