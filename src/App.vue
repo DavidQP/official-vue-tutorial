c3dc19e12ece5f5f0d33c5d22e45822ae84e80a2
david <juda9323@gmail.com>
2025-02-06 12:03:14 -0500

    Add attributes binding section

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 import { ref } from 'vue';
 import GettingStarted from '@/components/GettingStarted.vue'
 import DeclarativeRendering from '@/components/DeclarativeRendering.vue'
+import AttributesBinding from '@/components/AttributesBinding.vue'
 const section = ref(0);
 </script>
 
@@ -10,4 +11,5 @@ const section = ref(0);
   <h1>Vue official tutorial</h1>
   <GettingStarted :section="1" />
   <DeclarativeRendering :section="2" />
+  <AttributesBinding :section="3" />
 </template>
---
+++ src/components/AttributesBinding.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  section: {
+    type: Number,
+    default: 0
+  }
+})
+
+const title = ref('Attributes Binding');
+const titleClass = ref('title');
+</script>
+
+<template>
+  <h2>{{ section }} - {{ title }}</h2>
+  <p :class="titleClass">Make me red!</p>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
